2f210c9309718470ffeda6acbf2f92d849124c54
iago-s <iagooliveira3845@hotmail.com>
2021-07-01 10:32:33 -0300

    feat: Using bootstrap and partials ejs

--- index.js
+++ index.js
@@ -5,32 +5,12 @@ const PORT = 8080;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/:name/:lang', (req, res) => {
-  var { name, lang } = req.params;
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
-  var showMsg = true;
-
-  var products = [
-    {
-      name: 'Notebook',
-      price: 1199.99,
-    },
-    {
-      name: 'Phone',
-      price: 599.99,
-    },
-    {
-      name: 'Koke',
-      price: 3.99,
-    },
-  ];
-
-  res.render('index', {
-    name,
-    lang,
-    showMsg,
-    products,
-  });
+app.get('/ask', (req, res) => {
+  res.render('ask');
 });
 
 app.listen(PORT, () =>
